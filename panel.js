27710aa193076b0ca00c3c69712472661d25d9ed
Nina Satragno <nsatragno@chromium.org>
2019-10-23 13:36:31 -0400

    Remove "credentials" column for now.

--- panel.js
+++ panel.js
@@ -48,7 +48,6 @@ let renderAuthenticator = authenticator => {
     <td class="align-center">${authenticator.options.hasResidentKey}</td>
     <td class="align-center">${authenticator.options.hasUserVerification}</td>
     <td class="align-center">${authenticator.options.automaticPresenceSimulation}</td>
-    <td class="align-right">0</td>
     <td class="align-center">
       <button id="remove-${authenticator.id}">Remove</button>
     </td>
